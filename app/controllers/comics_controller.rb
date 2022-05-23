1d3ed8ec911b4a08f396a6885b9cdee4589acaf3
Einsen Vásquez <33559353+ZarakiLancelot@users.noreply.github.com>
2022-05-23 05:33:14 -0600

    Búsqueda por numero de comic

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -6,11 +6,32 @@ class ComicsController < ApplicationController
   def index
     @api_url = XkcdComics::Application::URL_XKCD
 
-    @peticion = Faraday.get("#{@api_url}info.0.json")
+    if params[:search_by_number] && params[:search_by_number] != ""
+      @peticion = Faraday.get("#{@api_url}#{params[:search_by_number]}/info.0.json")
+    else
+      @peticion = Faraday.get("#{@api_url}info.0.json")
+    end
 
     @comic = JSON.parse(@peticion.body)
     Rails.logger.info "="*150
     Rails.logger.info @comic
+
+    @max_number = @comic['num']
+    Rails.logger.info "*"*150
+    Rails.logger.info @max_number
+  end
+
+  def preview
+    @max_number = @max_number - 1
+    @peticion_anterior = Faraday.get("#{@api_url}#{@max_number}/info.0.json")
+
+    @comic = JSON.parse(@peticion_anterior.body)
+    Rails.logger.info "="*150
+    Rails.logger.info @max_number
+    Rails.logger.info @comic
+  end
+
+  def next
   end
 
   # GET /comics/1 or /comics/1.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :comics
+  resources :comics do
+    get 'next', to: 'comics#next'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
